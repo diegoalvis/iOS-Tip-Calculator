0613ae0036b8efeb73cb684c29eff8049f952fe5
Diego Alvis <diegoalvispal@gmail.com>
2020-06-20 04:25:22 -0500

    Implement tip calculator example

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billValueField: UITextField!
+    @IBOutlet weak var tipPercentageField: UITextField!
+    
+    @IBOutlet weak var tipValue: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    @IBAction func calculateTotals(_ sender: Any) {
+        if(billValueField.text?.isEmpty == true || tipPercentageField.text?.isEmpty == true) {
+            return
+        }
+        
+        let billValue = Double(billValueField.text!)!
+        let tipPercentage = Double(tipPercentageField.text!)!
+        
+        let tip = billValue * (tipPercentage / 100)
+        let total = billValue + tip
+        
+        tipValue.text = "Tip $\(tip)"
+        totalAmount.text = "Total $\(total)"
+    }
 
 }
 
