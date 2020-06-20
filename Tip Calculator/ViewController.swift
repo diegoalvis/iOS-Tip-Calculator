936f25d7f5ba9563b68ebe0cf32a6fce2231f61a
Diego Alvis <diegoalvispal@gmail.com>
2020-06-20 03:17:34 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Diego Alejandro Alvis Palencia on 20.06.20.
+//  Copyright © 2020 Diego Alejandro Alvis Palencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
